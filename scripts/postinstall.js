7986cffde06931110a6d9f73ac452165884214e2
Túlio <tulio.lira@outlook.com>
2024-10-30 00:22:52 -0300

    update postinstall

---
+++ scripts/postinstall.js
@@ -0,0 +1,2 @@
+console.log('Please add the following to your package.json scripts:');
+console.log('"crud": "plop --plopfile node_modules/crud-generator/plopfile.js"');
--- scripts/postinstall.ts
+++
@@ -1,2 +0,0 @@
-console.log('Please add the following to your package.json scripts:');
-console.log('"crud": "plop --plopfile node_modules/crud-generator/plopfile.js"');
