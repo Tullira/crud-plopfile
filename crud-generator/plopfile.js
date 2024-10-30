d431212f050dc91282f5c3d6eed9c378895c6aed
Túlio <tulio.lira@outlook.com>
2024-10-30 08:16:19 -0300

    fixed relative route

--- crud-generator/plopfile.js
+++ crud-generator/plopfile.js
@@ -12,41 +12,41 @@ module.exports = (plop) => {
     actions: [
       {
         type: "add",
-        path: "../src/controllers/{{pascalCase name}}Controller.ts",
+        path: "../../../src/controllers/{{pascalCase name}}Controller.ts",
         templateFile: "templates/regularController.ts.hbs",
       },
       {
         type: "add",
-        path: "../src/repositories/{{pascalCase name}}Repository.ts",
+        path: "../../../src/repositories/{{pascalCase name}}Repository.ts",
         templateFile: "templates/regularRepository.ts.hbs",
       },
       {
         type: "add",
-        path: "../src/routes/{{pascalCase name}}Routes.ts",
+        path: "../../../src/routes/{{pascalCase name}}Routes.ts",
         templateFile: "templates/regularRoutes.ts.hbs",
       },
       {
         type: "add",
-        path: "../src/DTOs/{{pascalCase name}}.ts",
+        path: "../../../src/DTOs/{{pascalCase name}}.ts",
         templateFile: "templates/regularDTO.ts.hbs",
       },
       {
         type: "modify",
-        path: "../src/DTOs/index.ts",
+        path: "../../../src/DTOs/index.ts",
         pattern: /^(import .*;\n)/,
         template:
           "import { {{pascalCase name}}, Update{{pascalCase name}} } from './{{pascalCase name}}';\n$1",
       },
       {
         type: "modify",
-        path: "../src/controllers/index.ts",
+        path: "../../../src/controllers/index.ts",
         pattern: /(export {)/,
         template:
           "import {{pascalCase name}}Controller from './{{pascalCase name}}Controller';\n\n$1",
       },
       {
         type: "modify",
-        path: "../src/repositories/index.ts",
+        path: "../../../src/repositories/index.ts",
         pattern: /(export {)/,
         template:
           "import {{pascalCase name}}Repository from './{{pascalCase name}}Repository';\n\n$1",
@@ -54,33 +54,33 @@ module.exports = (plop) => {
 
       {
         type: "append",
-        path: "../src/controllers/index.ts",
+        path: "../../../src/controllers/index.ts",
         template: "export { {{pascalCase name}}Controller };\n",
         skipIfExists: true,
       },
       {
         type: "append",
-        path: "../src/repositories/index.ts",
+        path: "../../../src/repositories/index.ts",
         template: "export { {{pascalCase name}}Repository };\n",
         skipIfExists: true,
       },
       {
         type: "append",
-        path: "../src/DTOs/index.ts",
+        path: "../../../src/DTOs/index.ts",
         template:
           "export { {{pascalCase name}}, Update{{pascalCase name}} };\n",
         skipIfExists: true,
       },
       {
         type: "modify",
-        path: "../src/routes/index.ts", // O mesmo arquivo
+        path: "../../../src/routes/index.ts", // O mesmo arquivo
         pattern: /\n/, // Localização específica
         template:
           "\n\nimport {{pascalCase name}}Routes from './{{pascalCase name}}Routes';",
       },
       {
         type: "modify",
-        path: "../src/routes/index.ts", // O mesmo arquivo
+        path: "../../../src/routes/index.ts", // O mesmo arquivo
         pattern: /const router = Router\(\);/, // Padrão para encontrar a linha do router.use
         template:
           "const router = Router();\n\nrouter.use('/{{camelCase name}}', {{pascalCase name}}Routes);",
